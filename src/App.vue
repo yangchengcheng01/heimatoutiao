6b6cebce6d5b5c7530900e2eed398583d72b8675
yangchengcheng01 <18906371466@163.com>
2022-09-14 16:53:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- 路由的出口 -->
+    <div>
+    
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less"></style>
